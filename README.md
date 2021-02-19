d73fd5f58b19b0b7717b9038224faf7cf0ec435a
Ondrej Franek <ondrej.franek@hellit.cz>
2021-02-19 08:43:33 +0100

    first init

---
+++ README.md
@@ -0,0 +1,14 @@
+![](https://img.shields.io/badge/Foundry-v0.7.0-informational)
+
+# Foundryvtt-dnd5e-lang-cs-CZ
+
+* **Version**: 0.0.11
+* **Foundry VTT Compatibility**: 0.7.9
+* **Game System Compatibility**: 1.2.4
+* **Module Requirements**: 
+
+## Description
+
+
+## Installation
+
---
+++ module/babele-register.js
@@ -0,0 +1,35 @@
+Hooks.once('init', () => {
+	game.settings.register("dnd5e_cs-CZ", "show-original-name", {
+		name: "Monster czech",
+		hint: "",
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean,
+		onChange: _ => window.location.reload()
+	});
+
+	if (typeof Babele !== 'undefined') {
+		Babele.get().register({
+			module: 'dnd5e_cs-CZ',
+			lang: 'cs',
+			dir: 'compendium'
+		});
+
+		if (!game.settings.get("dnd5e_cs-CZ", "show-original-name")) return;
+		TranslatedCompendium.prototype.translateOrigin = TranslatedCompendium.prototype.translate;
+		TranslatedCompendium.prototype.translate = function(data) {
+			let originalName = data.name;
+			let translatedData = this.translateOrigin(data);
+			if (originalName !== translatedData.name){
+				translatedData.name = translatedData.name + ' ' + originalName;
+			}
+			return translatedData;
+		};
+		TranslatedCompendium.prototype.i18nNameOrigin = TranslatedCompendium.prototype.i18nName;
+		TranslatedCompendium.prototype.i18nName = function(idx) {
+			let translated = this.i18nNameOrigin(idx);
+			return translated === idx.name ? translated : translated + ' ' + idx.name;
+		};
+	}
+});
